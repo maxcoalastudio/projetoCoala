ca776e44da9f46b12fe900996e0a410ff715395a
maxcoalastudio <maxwell24121992@hotmail.com>
2024-11-12 18:25:42 -0300

    criando uma engine usando python

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def main():
     #criando a janela(largura e altura, nome da janela, monitor, janela compartilhada)
     window = glfw.create_window(500,500,'Coala Engine Ver0.0.1', None, None)
     glfw.make_context_current(window)#cria um contexto atual para a janela 
-    while not glfw.window_should_close(window):
-        glfw.poll_events()
-        glfw.swap_buffers(window)
-    glfw.terminate()
+    while not glfw.window_should_close(window):#enquanto nao verdadeira o evento do X a janela continua executando em loop
+        glfw.poll_events()#trata eventos de cliques de botões, mouse , teclado , essa função interrompe o loop
+        glfw.swap_buffers(window)#2 framebuffers são usados, um pra background e outro pra desenhar 
+    glfw.terminate()#termina o glfw assim que o loop da janela termina ao apertar o X
 if __name__ == '__main__':
     main()
