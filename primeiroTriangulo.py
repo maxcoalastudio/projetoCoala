e0e476b77b243d84b0b1f656c502a7783999c596
maxcoalastudio <maxwell24121992@hotmail.com>
2024-11-19 18:17:14 -0300

    criando uma primitiva passando por um unico for

--- primeiroTriangulo.py
+++ primeiroTriangulo.py
@@ -1,23 +1,28 @@
 import glfw
 from OpenGL.GL import * 
 
+vertices = [
+    [-0.5, -0.5],
+    [0.5, -0.5],
+    [0.0, 0.3]
+]
+cores = [
+    [1,0,0],
+    [0,1,0],
+    [0,0,1]
+]
+
 #Função PARA CONFIGURAÇÕES INICIAIS DA MINHA aplicação
 def init():
     glClearColor(1, 1, 1, 1)#cor do fundo da janela
 #função para atualizar a renderização da cena
 def render():
-    glClear(GL_COLOR_BUFFER_BIT)#limpando o buffer dos pixels
-
-    glColor3f(1,0,0)                                                                    #definindo a cor para nossa geometria
-    glBegin(GL_TRIANGLES)
-    glColor3f(1,0,0)
-    glVertex2f(-0.5, -0.5)
-    glColor3f(0,1,0)
-    glVertex2f(0.5, -0.5)
-    glColor3f(0,0,1)
-    glVertex2f(0.0, 0.3)
-    glEnd()
-
+    glClear(GL_COLOR_BUFFER_BIT)                                                        #limpando o buffer dos pixels
+    glBegin(GL_TRIANGLES)                                                               #função desenha inicio com um parametro de primitiva
+    for v, c in zip(vertices, cores):                                                   #for pela lista de  lista de vertice e cores simuntaneamente e fomando pares com o valor do indice de cada lista
+        glColor3fv(c)                                                                   #colore cada vertice em sua sequencia 
+        glVertex2fv(v)                                                                  #desenhando cada vertice em seu lugar 
+    glEnd()                                                                             #função desenha final
 
 #Função Principal
 def main():
@@ -26,7 +31,7 @@ def main():
     window = glfw.create_window(500,500, 'Coala Engine Ver0.0.1', None, None)
     glfw.make_context_current(window)                                                   #CRIANDO O CONTEXTO OpenGL da janela
     init()
-    while not glfw.window_should_close(window):                                          #Enquanto a janela nao é fechada
+    while not glfw.window_should_close(window):                                         #Enquanto a janela nao é fechada
         glfw.poll_events()                                                              #tratamento de eventos
         render()	    
         glfw.swap_buffers(window)                                                       #troca de frame buffer(informação de cores dos pixels)
