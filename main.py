0389db7aa28fef0ede395f89fdc3409c8b423906
maxcoalastudio <maxwell24121992@hotmail.com>
2024-11-20 16:18:32 -0300

    funções do mouse, camera, e cursor

--- main.py
+++ main.py
@@ -6,19 +6,32 @@ from numpy import cross
 import time
 from PrimitiveObjects import PrimitiveObjects
 import inputs
+from math import cos, sin, pi, radians
+from numpy import linalg   
 
-
+#tela 
+width, height = 1600, 1200
 #variaveis da camera
 camera_pos = array([0.0, 0.0, 3])
 camera_front = array([0.0, 0.0, -1.0])
 camera_up = array([0.0, 1.0, 0.0])
 camera_speed = 0.001
+yaw, pitch = -90, 0.0 # rotação eixo y, eixo x para primeira pessoa
+keys={}
+
+#variaveis do mouse
+first_mouse = True
+cursor_disabled  = False
+esc_pressed = False
+sensibility = 0.1
+last_x, last_y = width/2, height/2
+
 
 #Função Principal
 def main():
     glfw.init()
     #criando a janela(largura e altura, nome da janela, monitor, janela compartilhada)
-    window = glfw.create_window(800,600,'Coala Engine Ver0.0.1', None, None)
+    window = glfw.create_window(width,height,'Coala Engine Ver0.0.1', None, None)
     glfw.make_context_current(window)#cria um contexto atual para a janela 
 
     #ativando o blend do alpha
@@ -35,7 +48,7 @@ def main():
     glMatrixMode(GL_MODELVIEW) #modelo de visualização
     
     
-    keys={}
+    
     def camera(cp , cf , cup):
         #global camera_pos, camera_front, camera_up
         camera_pos = cp
@@ -53,22 +66,68 @@ def main():
             keys[key] = False
     
     def process_input():
-        global camera_pos, camera_front, camera_up, camera_speed
+        global camera_pos, camera_front, camera_up, camera_speed, esc_pressed, first_mouse, cursor_disabled
         
         if keys.get(glfw.KEY_W, False):
             camera_pos += camera_speed * camera_front
         if keys.get(glfw.KEY_S, False):
             camera_pos -= camera_speed * camera_front
         if keys.get(glfw.KEY_A, False):
-            camera_pos += cross(camera_front, camera_up) * camera_speed
-        if keys.get(glfw.KEY_D, False):
             camera_pos -= cross(camera_front, camera_up) * camera_speed
+        if keys.get(glfw.KEY_D, False):
+            camera_pos += cross(camera_front, camera_up) * camera_speed
+
+        if glfw.get_key(window, glfw.KEY_ESCAPE) == glfw.PRESS and not esc_pressed:
+            cursor_disabled = not cursor_disabled
+            mode = glfw.CURSOR_DISABLED if cursor_disabled else glfw.CURSOR_NORMAL 
+            glfw.set_input_mode(window, glfw.CURSOR, mode)
+            esc_pressed = True
+            first_mouse = cursor_disabled
+            if not cursor_disabled:
+                glfw.set_cursor_pos(window, last_x, last_y)
+        elif glfw.get_key(window, glfw.KEY_ESCAPE) == glfw.RELEASE:
+            esc_pressed = False
+
+    def mouse_callback(window, xPos, yPos):
+        global yaw, pitch, last_x, last_y, first_mouse, camera_front, cursor_disabled, sensibility
+        if not cursor_disabled:
+             return
+        if first_mouse:
+            last_x = xPos
+            last_y = yPos
+            first_mouse = False
+        xOffset = xPos - last_x
+        yOffset = last_y - yPos
+        last_x = xPos
+        last_y = yPos
+        xOffset *= sensibility
+        yOffset *= sensibility
+        yaw += xOffset
+        pitch += yOffset
+
+
+
+        #angulo maxximo de pitch para subir e descer a camera primeira pessoa
+        if pitch > 89.0:
+            pitch = 89.0
+        if pitch < -89.0:
+            pitch = -89.0
+        
+        direction = array([
+            cos(radians(yaw)) * cos(radians(pitch)),
+            sin(radians(pitch)),
+            sin(radians(yaw)) * cos(radians(pitch))
+        ])
+
+        camera_front = direction / linalg.norm(direction) 
 
     #aqui chama os objetos primitivos da classe PrimitiveObjects
     cubo = PrimitiveObjects()
 
     glClearColor(0.3, 0.3, 0.3, 1.0)
     glfw.set_key_callback(window, key_callback)
+    glfw.set_cursor_pos_callback(window, mouse_callback)
+    glfw.set_input_mode(window, glfw.CURSOR, glfw.CURSOR_DISABLED)
     angle = 0
     direction = 0
     speed = 1
