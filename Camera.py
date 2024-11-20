4be7ed277e37272917cefb5192d531cedb7e729e
maxcoalastudio <maxwell24121992@hotmail.com>
2024-11-20 17:31:38 -0300

    criando uma classe só para a camera

---
+++ Camera.py
@@ -0,0 +1,89 @@
+import glfw
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from numpy import array
+from numpy import cross
+from math import cos, sin, pi, radians
+from numpy import linalg 
+class Camera:
+
+    def __init__(self, width, height):
+        
+        #variaveis da camera
+        self.camera_pos = array([0.0, 0.0, 3])
+        self.camera_front = array([0.0, 0.0, -1.0])
+        self.camera_up = array([0.0, 1.0, 0.0])
+        self.camera_speed = 0.001
+        self.yaw, self.pitch = -90, 0.0 # rotação eixo y, eixo x para primeira pessoa
+        self.keys={}
+
+        #variaveis do mouse
+        self.first_mouse = True
+        self.cursor_disabled  = False
+        self.esc_pressed = False
+        self.sensibility = 0.1
+        self.last_x, self.last_y = width/2, height/2
+
+
+    def updateCamera(self):
+        glLoadIdentity()
+        camera_target = self.camera_pos + self.camera_front
+        gluLookAt(self.camera_pos[0], self.camera_pos[1], self.camera_pos[2], camera_target[0], camera_target[1], camera_target[2], self.camera_up[0], self.camera_up[1], self.camera_up[2])# onde a camera se encontra para onde ela olha, e a orientação dela
+
+    # movimentando a camera 
+    def key_callback(self, window, key, scancode, action, mods):
+        if action == glfw.PRESS:
+            self.keys[key] = True
+        elif action == glfw.RELEASE:
+            self.keys[key] = False
+    
+    def process_input(self, window):
+        if self.keys.get(glfw.KEY_W, False):
+            self.camera_pos += self.camera_speed * self.camera_front
+        if self.keys.get(glfw.KEY_S, False):
+            self.camera_pos -= self.camera_speed * self.camera_front
+        if self.keys.get(glfw.KEY_A, False):
+            self.camera_pos -= cross(self.camera_front, self.camera_up) * self.camera_speed
+        if self.keys.get(glfw.KEY_D, False):
+            self.camera_pos += cross(self.camera_front, self.camera_up) * self.camera_speed
+
+        if glfw.get_key(window, glfw.KEY_ESCAPE) == glfw.PRESS and not self.esc_pressed:
+            self.cursor_disabled = not self.cursor_disabled
+            mode = glfw.CURSOR_DISABLED if self.cursor_disabled else glfw.CURSOR_NORMAL 
+            glfw.set_input_mode(window, glfw.CURSOR, mode)
+            self.esc_pressed = True
+            self.first_mouse = self.cursor_disabled
+            if not self.cursor_disabled:
+                glfw.set_cursor_pos(window, self.last_x, self.last_y)
+        elif glfw.get_key(window, glfw.KEY_ESCAPE) == glfw.RELEASE:
+            self.esc_pressed = False
+
+    def mouse_callback(self, window, xPos, yPos):
+        if not self.cursor_disabled:
+             return
+        if self.first_mouse:
+            self.last_x = xPos
+            self.slast_y = yPos
+            self.first_mouse = False
+        xOffset = xPos - self.last_x
+        yOffset = self.last_y - yPos
+        self.last_x = xPos
+        self.last_y = yPos
+        xOffset *= self.sensibility
+        yOffset *= self.sensibility
+        self.yaw += xOffset
+        self.pitch += yOffset
+
+        #angulo maxximo de pitch para subir e descer a camera primeira pessoa
+        if self.pitch > 89.0:
+            self.pitch = 89.0
+        if self.pitch < -89.0:
+            self.pitch = -89.0
+        
+        self.direction = array([
+            cos(radians(self.yaw)) * cos(radians(self.pitch)),
+            sin(radians(self.pitch)),
+            sin(radians(self.yaw)) * cos(radians(self.pitch))
+        ])
+
+        self.camera_front = self.direction / linalg.norm(self.direction) 
--- main.py
+++ main.py
@@ -2,31 +2,11 @@ import glfw
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from numpy import array
-from numpy import cross
 import time
 from PrimitiveObjects import PrimitiveObjects
-import inputs
-from math import cos, sin, pi, radians
-from numpy import linalg   
-
-#tela 
+from Camera import Camera
+#tela
 width, height = 1600, 1200
-#variaveis da camera
-camera_pos = array([0.0, 0.0, 3])
-camera_front = array([0.0, 0.0, -1.0])
-camera_up = array([0.0, 1.0, 0.0])
-camera_speed = 0.001
-yaw, pitch = -90, 0.0 # rotação eixo y, eixo x para primeira pessoa
-keys={}
-
-#variaveis do mouse
-first_mouse = True
-cursor_disabled  = False
-esc_pressed = False
-sensibility = 0.1
-last_x, last_y = width/2, height/2
-
-
 #Função Principal
 def main():
     glfw.init()
@@ -48,90 +28,18 @@ def main():
     glMatrixMode(GL_MODELVIEW) #modelo de visualização
     
     
-    
-    def camera(cp , cf , cup):
-        #global camera_pos, camera_front, camera_up
-        camera_pos = cp
-        camera_front = cf
-        camera_up = cup
-        glLoadIdentity()
-        camera_target = camera_pos + camera_front
-        gluLookAt(camera_pos[0], camera_pos[1], camera_pos[2], camera_target[0], camera_target[1], camera_target[2], camera_up[0], camera_up[1], camera_up[2])# onde a camera se encontra para onde ela olha, e a orientação dela
-
-    # movimentando a camera 
-    def key_callback(window, key, scancode, action, mods):
-        if action == glfw.PRESS:
-            keys[key] = True
-        elif action == glfw.RELEASE:
-            keys[key] = False
-    
-    def process_input():
-        global camera_pos, camera_front, camera_up, camera_speed, esc_pressed, first_mouse, cursor_disabled
-        
-        if keys.get(glfw.KEY_W, False):
-            camera_pos += camera_speed * camera_front
-        if keys.get(glfw.KEY_S, False):
-            camera_pos -= camera_speed * camera_front
-        if keys.get(glfw.KEY_A, False):
-            camera_pos -= cross(camera_front, camera_up) * camera_speed
-        if keys.get(glfw.KEY_D, False):
-            camera_pos += cross(camera_front, camera_up) * camera_speed
-
-        if glfw.get_key(window, glfw.KEY_ESCAPE) == glfw.PRESS and not esc_pressed:
-            cursor_disabled = not cursor_disabled
-            mode = glfw.CURSOR_DISABLED if cursor_disabled else glfw.CURSOR_NORMAL 
-            glfw.set_input_mode(window, glfw.CURSOR, mode)
-            esc_pressed = True
-            first_mouse = cursor_disabled
-            if not cursor_disabled:
-                glfw.set_cursor_pos(window, last_x, last_y)
-        elif glfw.get_key(window, glfw.KEY_ESCAPE) == glfw.RELEASE:
-            esc_pressed = False
-
-    def mouse_callback(window, xPos, yPos):
-        global yaw, pitch, last_x, last_y, first_mouse, camera_front, cursor_disabled, sensibility
-        if not cursor_disabled:
-             return
-        if first_mouse:
-            last_x = xPos
-            last_y = yPos
-            first_mouse = False
-        xOffset = xPos - last_x
-        yOffset = last_y - yPos
-        last_x = xPos
-        last_y = yPos
-        xOffset *= sensibility
-        yOffset *= sensibility
-        yaw += xOffset
-        pitch += yOffset
-
-
-
-        #angulo maxximo de pitch para subir e descer a camera primeira pessoa
-        if pitch > 89.0:
-            pitch = 89.0
-        if pitch < -89.0:
-            pitch = -89.0
-        
-        direction = array([
-            cos(radians(yaw)) * cos(radians(pitch)),
-            sin(radians(pitch)),
-            sin(radians(yaw)) * cos(radians(pitch))
-        ])
-
-        camera_front = direction / linalg.norm(direction) 
+    camera = Camera(width, height)
 
     #aqui chama os objetos primitivos da classe PrimitiveObjects
     cubo = PrimitiveObjects()
 
     glClearColor(0.3, 0.3, 0.3, 1.0)
-    glfw.set_key_callback(window, key_callback)
-    glfw.set_cursor_pos_callback(window, mouse_callback)
+    glfw.set_key_callback(window, camera.key_callback)
+    glfw.set_cursor_pos_callback(window, camera.mouse_callback)
     glfw.set_input_mode(window, glfw.CURSOR, glfw.CURSOR_DISABLED)
     angle = 0
-    direction = 0
+    dir = 0
     speed = 1
-
     # Variáveis para calcular o FPS
     frame_count = 0
     start_time = time.time()
@@ -145,22 +53,19 @@ def main():
             frame_count = 0
             start_time = current_time
         return frame_count, start_time
-       
 
     while not glfw.window_should_close(window):#enquanto nao verdadeira o evento do X a janela continua executando em loop
         glfw.poll_events()#trata eventos de cliques de botões, mouse , teclado , essa função interrompe o loop
-
-        process_input()
+        camera.process_input(window)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)#limpa a tela com a cor definida em glClearColor e usa a distacia da camera como profundidade pra desenhar as faces mais procimas por ultimo
         # Calcular FPS
         frame_count, start_time = calculate_fps(frame_count, start_time)
         glLoadIdentity()
-        #glTranslatef(0, 0, -direction* speed)#movendo o cubo para dentro no eixo -z
-
+        #glTranslatef(0, 0, -dir* speed)#movendo o cubo para dentro no eixo -z
         #glRotatef(angle, 1, 1, 1)#ligando a rotação pra cada eixo
 
         #criando a camera
-        camera(camera_pos, camera_front, camera_up)
+        camera.updateCamera()
         #chamando as figuras bidimensionais
         #quads()#desenhando o quadrado
         #tris()#desenhando o triangulo
@@ -170,9 +75,8 @@ def main():
         #esfera(0.08, 20, 20)
         #fazendo o cubo rotacionar pelo angulo acrescido
 
-        angle +=  0.1
-        direction = 3 
-
+        #angle +=  0.1
+        #dir+= 0.04
 
         glfw.swap_buffers(window)#2 framebuffers são usados, um pra background e outro pra desenhar 
         
